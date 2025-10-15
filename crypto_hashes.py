523bab2b98f9294e6d097a8851ae7b02e05efe13
Gary Yim <89404326+Gary055@users.noreply.github.com>
2025-10-15 10:56:38 -0700

    code imports

---
+++ crypto_hashes.py
@@ -0,0 +1,7 @@
+from random import randint
+from Crypto.Hash import SHA256
+from operator import xor
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+from os import urandom
+
